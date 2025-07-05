f6c1a12997b8e8878dd9e75e06c0df148483a644
bennweb <bennbickman@gmail.com>
2025-07-05 19:09:21 +0530

    Delete database/mongo.py

--- database/mongo.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URL = "mongodb+srv://itachii130506:W0Qif2solCNPDyUd@bickman.jaumcw3.mongodb.net/?retryWrites=true&w=majority&appName=Bickman"
-
-client = MongoClient(MONGO_URL)
-db = client["bennbickman"]
-
-users = db["users"]
