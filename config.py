d18bbe5afbd9aceeeed9e25eda1d8123cb47103a
bennweb <bennbickman@gmail.com>
2025-07-05 16:53:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID = 21283098
+API_HASH = "a272cca2d51cc3d3fc74a28d2df77951"
+BOT_TOKEN = "7189411720:AAEjhH5HPV-EgGejh6_eRh7dEz9QYmb2J_0"
+MONGO_URL = "mongodb+srv://itachii130506:W0Qif2solCNPDyUd@bickman.jaumcw3.mongodb.net/?retryWrites=true&w=majority&appName=Bickman"
+LOG_CHANNEL = -1002546389259
